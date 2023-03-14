1b78e22fad1341054e8d2e389138db03b92d2bab
rafamininel <rafaelmininel@icloud.com>
2023-03-13 23:02:31 -0300

    update javascript

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@
 
 // passo 1 - dar um jeito de pegar o elemento HTML dos botões
 const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
 
 
 // passo 2 - identificar o clique do usuário no botão
@@ -16,13 +17,41 @@ const botoesCarrossel = document.querySelectorAll('.botao');
 // botao2.addEventListener('click', () => {
 //     console.log('clicou no botao 2');
 // })
-botoesCarrossel.forEach((botao) => {
+botoesCarrossel.forEach((botao, indice) => {
     botao.addEventListener('click', () => {
+
+        console.log(indice);
+
         // passo 3 - desmarcar o botão selecionado anterior
-        const botaoSelecionado = document.querySelector('.selecionado');
-        botaoSelecionado.classList.remove('selecionado');
+        desativarBotaoSelecionado();
 
         // passo 4 - marcar o botão clicado como se estivesse selecionada
+        selecionarBotaoCarrossel(botao);
+
+        // passo 5 - esconder a imagem ativa de fundo
+        esconderImagemAtiva();
+
+        // passo 6 - mostrar a imagem de fundo do botão clicado
+        mostrarImagemDeFundo(indice);
     })
 })
+
+function mostrarImagemDeFundo(indice) {
+    console.log(imagens);
+    imagens[indice].classList.add('ativa');
+}
+
+function selecionarBotaoCarrossel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
 
